1c50fc7002e2cdf909379fd5d9ebe020b0d87e6b
Mathias Bergquist <berka28@hotmail.com>
2020-09-29 10:53:03 +0200

    Up1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Models;
+using SharedLibraries.Services;
+using System;
 
 namespace ConsoleApp
 {
     class Program
     {
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=EC-WIN20-MB-IoT-hubb-1.azure-devices.net;DeviceId=ConsoleApp;SharedAccessKey=KhOzfu1bdjGysi3hmNSD+lA7RBVHjjtoaThZ+c0RIyY=", TransportType.Mqtt);
+
+
         static void Main(string[] args)
         {
-            
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+
+            Console.ReadKey();
         }
     }
 }
